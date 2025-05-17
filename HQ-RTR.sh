595aca0b22a4fa1e9c537ff1f9a67775a288cf5f
get-lucky-now <bixbit.ceo@gmail.com>
2025-05-17 13:46:07 +0700

    Create HQ-RTR.sh

---
+++ HQ-RTR.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+# Переименовываем машину
+hostnamectl set-hostname isp.au-team.irpo
+
+# Создаем директории для интерфейсов
+mkdir -p /etc/net/ifaces/{ens19,ens19.100,ens19.200,ens19.999,iptunnel}
+
+# Настраиваем интерфейс ens19
+cat <<EOF > /etc/net/ifaces/ens19/options
+BOOTPROTO=static
+TYPE=eth
+EOF
+
+# Настраиваем интерфейс ens19.100 (VLAN 100)
+cat <<EOF > /etc/net/ifaces/ens19.100/options
+BOOTPROTO=static
+TYPE=vlan
+HOST=ens19
+VID=100
+EOF
+
+# Настраиваем интерфейс ens19.200 (VLAN 200)
+cat <<EOF > /etc/net/ifaces/ens19.200/options
+BOOTPROTO=static
+TYPE=vlan
+HOST=ens19
+VID=200
+EOF
+
+# Настраиваем интерфейс ens19.999 (VLAN 999)
+cat <<EOF > /etc/net/ifaces/ens19.999/options
+BOOTPROTO=static
+TYPE=vlan
+HOST=ens19
+VID=999
+EOF
+
+# Устанавливаем статические адреса для интерфейсов VLAN
+echo '192.168.1.1/26' > /etc/net/ifaces/ens19.100/ipv4address
+echo '192.168.2.1/28' > /etc/net/ifaces/ens19.200/ipv4address
+echo '192.168.99.1/29' > /etc/net/ifaces/ens19.999/ipv4address
+
+# Настраиваем интерфейс iptunnel (GRE)
+cat <<EOF > /etc/net/ifaces/iptunnel/options
+TYPE=iptun
+TUNTYPE=gre
+TUNLOCAL=172.16.4.2
+TUNREMOTE=172.16.5.2 
+EOF
+
+# Устанавливаем статический адрес и шлюз для интерфейса iptunnel
+echo '10.0.0.1/30' > /etc/net/ifaces/iptunnel/ipv4address
+echo '192.168.3.0/27 via 10.0.0.2' > /etc/net/ifaces/iptunnel/ipv4route
+
+# Включаем форвардинг пакетов
+sed -i 's/net.ipv4.ip_forward.*/net.ipv4.ip_forward = 1/' /etc/net/sysctl.conf
+
+sysctl -p
+
+# Очищаем существующие правила iptables и настраиваем NAT
+iptables -F
+iptables -t nat -A POSTROUTING -o ens18 -j MASQUERADE
+iptables-save > /etc/sysconfig/iptables
+
+# Перезапускаем сеть
+service network restart
+
+# Добавляем IPTABLES в автозапуск
+systemctl enable iptables
+systemctl start iptables
+
+apt-get install frr openssh-server chrony dnsmasq rsyslog-classic
+
+# Создаем нового пользователя
+useradd net_admin
+
+# Устанавливаем пароль для пользователя sshuser
+passwd net_admin
+
+# Редактируем файл sudoers, разрешая пользователю выполнять команды без пароля
+echo 'net_admin ALL=(ALL:ALL) NOPASSWD: ALL' >> /etc/sudoers
+
+cat <<EOF > /tmp/sshd_new_top
+Port 2024
+PermitRootLogin no
+AllowUsers net_admin
+MaxAuthTries 2
+Banner /etc/openssh/banner
+EOF
+
+cat /etc/openssh/sshd_config >> /tmp/sshd_new_top
+mv /tmp/sshd_new_top /etc/openssh/sshd_config
+
+# Создаем файл баннера входа
+cat <<EOF > /etc/openssh/banner
+Authorized access only
+
+EOF
+
+# Перезапускаем сервис SSHD
+systemctl restart sshd.service
+
+# Включаем OSPF
+sed -i 's/^ospfd=no/ospfd=yes/' /etc/frr/daemons
+
+systemctl restart frr
+
+cat <<EOF > /etc/frr/frr.conf
+frr version 9.0.2
+frr defaults traditional
+hostname isp.au-team.irpo
+log file /var/log/frr/frr.log
+no ipv6 forvarding
+!
+router ospf
+ network 10.0.0.0/30 area 0
+ network 172.16.4.0/28 area 0
+ network 172.16.5.0/28 area 0
+ network 192.168.1.0/26 area 0
+ network 192.168.2.0/28 area 0
+ network 192.168.99.0/29 area 0
+exit
+!
+EOF
